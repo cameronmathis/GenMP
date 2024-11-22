f28d18cfcfed730de201257ebbde8e7c70c9a7a3
Cameron Mathis <cmmathis98@gmail.com>
2024-11-22 00:21:41 -0500

    remove unused code

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,15 +1,12 @@
 import { Box } from '@mui/material';
 import React from 'react';
 
-import { useGMP } from '../../context/Context';
 import { ChatBox } from '../ChatBox/ChatBox';
 import { Input } from '../Input/Input';
 
 import styles from './Main.module.css';
 
 export function Main() {
-    const { state } = useGMP();
-
     return (
         <Box className={styles.main}>
             <ChatBox />
