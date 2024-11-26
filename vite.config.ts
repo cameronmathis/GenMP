ef8639ea0d5d4cc19e646e6c12c709806e83518d
Cameron Mathis <cmmathis98@gmail.com>
2024-11-26 11:59:43 -0500

    trying to fix gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import react from '@vitejs/plugin-react';
-import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
@@ -9,17 +8,4 @@ export default defineConfig({
         port: 80,
     },
     plugins: [react()],
-    resolve: {
-        alias: {
-            '@packages': path.resolve(__dirname, 'packages'),
-        },
-    },
-    optimizeDeps: {
-        exclude: ['/node_modules/'],
-    },
-    build: {
-        rollupOptions: {
-            external: [/node_modules/],
-        },
-    },
 });
